d65a6cae3df4a1178300410b592eeb186ee36d0f
gouthamigaddam <gouthamireddy255@gmail.com>
2023-07-27 14:05:28 -0400

    mongodb data - push

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@gmail.com',
+    password: bcrypt.hashSync('12345678', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Sai Charan',
+    email: 'scharan518@gmail.com',
+    password: bcrypt.hashSync('12345678', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'Vishal',
+    email: 'vishal@gmail.com',
+    password: bcrypt.hashSync('12345678', 10),
+    isAdmin: false,
+  },
+];
+
+export default users;

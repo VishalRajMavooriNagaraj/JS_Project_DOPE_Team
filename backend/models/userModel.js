11c27f759dc96e23f84a5006597290bcb8a05c9e
venkatesh <venkateshpinapala.ca@gmail.com>
2023-08-01 16:19:03 -0400

    user authentication functionality frontend&backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const reviewSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -10,5 +11,21 @@ const reviewSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model('User', reviewSchema);
+//function to compare user enter password with the password in MongoDB
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//this function executes before saving data into db
+userSchema.pre('save', async function (next) {
+  //if password is not modified continue with next middleware
+  if (!this.isModified('password')) {
+    next();
+  }
+  //else hash the plain text password
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
 export default User;
